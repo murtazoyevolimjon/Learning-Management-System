333ed2cc9032a6c825df3ddb7d2ab4c481b2d8c3
Olimjon Murtazoyev <murtazoyevolimjon54@gmail.com>
2026-01-23 16:29:37 +0500

    mentor-profile

--- src/modules/users/dto/login.dto.ts
+++ src/modules/users/dto/login.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginUserDto {
-    @ApiProperty({ example: '998997776655' })
+    @ApiProperty({ example: '+998997776655' })
     @IsNotEmpty()
     @IsString()
     phone: string;
